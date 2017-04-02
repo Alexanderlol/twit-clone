c9dc90827c30936ac1779bb13a601a014755b792
Alex Afeltra <afeltra.alex@gmail.com>
2017-04-02 08:17:40 -0400

    title helper

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,7 +4,7 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get static_pages_home_url
     assert_response :success
-    assert_select "title", "Home | Twit Clone"
+    assert_select "title", "Twit Clone"
   end
 
   test "should get help" do
