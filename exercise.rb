617b542dbd88f621e9c39c05b2f10d101be94d17
Alex Afeltra <afeltra.alex@gmail.com>
2017-04-06 18:55:45 -0400

    hash and symb exercises

--- exercise.rb
+++ exercise.rb
@@ -21,3 +21,40 @@ def string_shuffle(s)
 end
 
 puts string_shuffle("foobar")
+
+=begin
+-----------------------
+HASH AND SYMBOL EXERCISES
+-----------------------
+=end
+
+h1 = {one: "uno", two: "dos", three: "tres"}
+
+h1.each do |k,v|
+	puts "#{k} in spanish is #{v}"
+end
+
+person1 = { :first => "sandra", :last => "stj" }
+person2 = { :first => "alex", :last => "afe" }
+person3 = { :first => "jade", :last => "dog" }
+
+params = {}
+
+params[:mother] = person1
+params[:me] = person2
+params[:dog] = person3
+
+puts params[:mother][:first]
+puts params[:me][:first]
+puts params[:dog][:first]
+
+hash1 = { name: "alex", email: "alex@example.com", password: ('a'..'z').to_a.shuffle[0..15].join }
+
+puts hash1
+
+
+
+
+
+
+
