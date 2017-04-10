064bf96b5433becab8c2b01105c50ff3e47628fa
Alex Afeltra <afeltra.alex@gmail.com>
2017-04-09 21:33:28 -0400

    added email and password validations and tests

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -5,7 +5,8 @@ class UserTest < ActiveSupport::TestCase
   #   assert true
   # end
   def setup
-  	@user = User.new(name: "Example User", email: "user@example.com")
+  	@user = User.new(name: "Example User", email: "user@example.com",
+  									 password: "foobar", password_confirmation: "foobar")
   end
 
   test "should be valid" do
@@ -21,4 +22,61 @@ class UserTest < ActiveSupport::TestCase
   	@user.email = ""
   	assert_not @user.valid?
   end
+
+  test "name should not be too long" do
+  	@user.name = "a" * 51
+  	assert_not @user.valid?
+  end
+
+  test "email should not be too long" do
+  	@user.email = "a" * 244 + "@example.com"
+  	assert_not @user.valid?
+  end
+
+  test "email validation should accept valid addresses" do
+  	valid_addresses = %w[user@example.com USER@foo.COM A_US-ER@foo.bar.org
+  											 first.last@foo.jp alice+bob@baz.cn]
+  	valid_addresses.each do |valid_address|
+  		@user.email = valid_address
+  		assert @user.valid?, "#{valid_address.inspect} should be valid"
+  	end
+  end
+
+  test "email validation should reject invalid addresses" do
+  	invalid_addresses = %w[user@exmaple,com user_at_foo.org user.name@example.
+  												 fii@bar_baz.com foo@bar+baz.com]
+  	invalid_addresses.each do |invalid_address|
+  		@user.email = invalid_address
+  		assert_not @user.valid?, "#{invalid_address.inspect} should be invalid"
+  	end
+  end
+
+  test "email addresses should be unique" do
+  	duplicate_user = @user.dup
+  	duplicate_user.email = @user.email.upcase
+  	@user.save
+  	assert_not duplicate_user.valid?
+  end
+
+  test "password should be present (nonblank)" do
+  	@user.password = @user.password_confirmation = " " * 6
+  	assert_not @user.valid?
+  end
+
+  test "password should have a minimum length" do
+  	@user.password = @user.password_confirmation = "a" * 5
+  	assert_not @user.valid?
+  end
 end
+
+
+
+
+
+
+
+
+
+
+
+
