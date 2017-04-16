1fee522cbe640156d78a3b6330a67694ac797e1f
Alex Afeltra <afeltra.alex@gmail.com>
2017-04-16 08:54:29 -0400

    save valid user data and display flash

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,8 @@ class UsersController < ApplicationController
   def create
   	@user = User.new(user_params)
   	if @user.save
-
+  		flash[:success] = "Welcome to Twit Clone!"
+  		redirect_to @user
   	else
   		render 'new'
   	end
