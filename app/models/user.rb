c602566276a7c5ff6e6287cad26dadfa608091cf
Alex Afeltra <afeltra.alex@gmail.com>
2017-04-23 10:38:37 -0400

    remember user permanently with cookies

--- app/models/user.rb
+++ app/models/user.rb
@@ -21,9 +21,14 @@ class User < ApplicationRecord
 	def User.new_token
 		SecureRandom.urlsafe_base64
 	end
-
+	#remembers a user in the database for use in persistent sessions
 	def remember
-		self.rememeber_token = User.new_token
+		self.remember_token = User.new_token
 		update_attribute(:remember_digest, User.digest(remember_token))
+	end
+
+	#returns true if the given token matches the digest
+	def authenticated?(remember_token)
+		BCrypt::Password.new(remember_digest).is_password?(remember_token)
 	end
 end
