8e78aa718ad2d033713a693a38dac4f6f42c4de4
Alex Afeltra <afeltra.alex@gmail.com>
2017-04-24 17:14:22 -0400

    forget user session when logout

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -26,8 +26,16 @@ module SessionsHelper
 		!current_user.nil?
 	end
 
+	#forgets persistent session
+	def forget(user)
+		user.forget
+		cookies.delete(:user_id)
+		cookies.delete(:remember_token)
+	end
+
 	#logs out the current user
 	def log_out
+		forget(current_user)
 		session.delete(:user_id)
 		@current_user = nil
 	end
