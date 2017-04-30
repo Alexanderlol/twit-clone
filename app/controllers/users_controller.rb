fdde22b4d0b6b0ff5b2403f45123b31818441f22
Alex Afeltra <afeltra.alex@gmail.com>
2017-04-30 11:15:12 -0400

    can successful edit user profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,10 +21,16 @@ class UsersController < ApplicationController
 
   def edit
     @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
     if @user.update_attributes(user_params)
-      #handle success update
+      flash[:success] = "Profile updated"
+      redirect_to @user
     else
       render 'edit'
+    end
   end
 
   private
