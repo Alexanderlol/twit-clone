43977a100517bf459d9902b51ed36b8a0e0882df
Alex Afeltra <afeltra.alex@gmail.com>
2017-05-05 11:02:52 -0400

    mailer previews

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -5,7 +5,7 @@ class UserMailer < ApplicationMailer
   #
   #   en.user_mailer.account_activation.subject
   #
-  def account_activation
+  def account_activation(user)
     @user = user
     mail to: user.email, subject: "Account activation"
   end
