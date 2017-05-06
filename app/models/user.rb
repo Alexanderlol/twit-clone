890eb0456d76cb8ae2d8bf4bb598408746b4ca00
Alex Afeltra <afeltra.alex@gmail.com>
2017-05-06 09:16:59 -0400

    generalized authenticated? method

--- app/models/user.rb
+++ app/models/user.rb
@@ -29,9 +29,10 @@ class User < ApplicationRecord
 	end
 
 	#returns true if the given token matches the digest
-	def authenticated?(remember_token)
-		return false if remember_digest.nil?
-		BCrypt::Password.new(remember_digest).is_password?(remember_token)
+	def authenticated?(attribute, token)
+		digest = send("#{attribute}_digest")
+		return false if digest.nil?
+		BCrypt::Password.new(digest).is_password?(token)
 	end
 
 	#forget a user
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -69,7 +69,7 @@ class UserTest < ActiveSupport::TestCase
   end
 
   test "authenticated? should return false for a user with nil digest" do
-    assert_not @user.authenticated?('')
+    assert_not @user.authenticated?(:remember, '')
   end
 end
 
