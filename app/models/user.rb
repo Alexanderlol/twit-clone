fdd2aec240392be970bb0aa81aca7376a0c8ca61
Alex Afeltra <afeltra.alex@gmail.com>
2017-05-07 12:36:06 -0400

    create action and email previews

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-	attr_accessor :remember_token, :activation_token
+	attr_accessor :remember_token, :activation_token, :reset_token
 	before_save   :downcase_email
 	before_create :create_activation_digest
 
@@ -49,6 +49,18 @@ class User < ApplicationRecord
 	#send activation email
 	def send_activation_email
 		UserMailer.account_activation(self).deliver_now
+	end
+
+	#sets the password reset attributes
+	def create_reset_digest
+		self.reset_token = User.new_token
+		update_attribute(:reset_digest, User.digest(reset_token))
+		update_attribute(:reset_sent_at, Time.zone.now)
+	end
+
+	#sends password reset email
+	def send_password_reset_email
+		UserMailer.password_reset(self).deliver_now
 	end
 
 	private
