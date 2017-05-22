7f61dd525a0f83ad1979019334d56707c24134e5
Alex Afeltra <afeltra.alex@gmail.com>
2017-05-22 15:36:51 -0400

    microposts integration test

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -26,7 +26,7 @@ class MicropostsController < ApplicationController
 		end
 
 		def correct_user
-			@micropost = current_user.micropost.find_by(id: params[:id])
-			redirect_to root_url if @micropost.nill?
+			@micropost = current_user.microposts.find_by(id: params[:id])
+			redirect_to root_url if @micropost.nil?
 		end
 end
